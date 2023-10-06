4defb5007df704a9328c413153fde6608ce6aaa2
tuankthai <ttlive4jc@gmail.com>
2023-10-06 15:00:57 -0700

    add star icon to Yacys navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,17 @@ export default function Nav() {
 
     return (
         <div className='navbar'>
-            <p>Yacy's</p>
+            <div className='star-div'>
+
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="star-svg" viewBox="0 0 16 16">
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg>
+                {/* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="red" class="bi bi-star-fill" viewBox="0 0 16 16">
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                </svg> */}
+                <p>Yacy's</p>
+            </div>
+
             <Link to="/Login?src=navbar" className='navbarlinks'>
                 {/* <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
                     <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
@@ -25,5 +35,5 @@ export default function Nav() {
                 </div>
             </Link>
         </div>
-    )  
+    )
 }
